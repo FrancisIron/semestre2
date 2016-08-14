29ebd55d34d4a607e4dfca5c58dc6c54a025900c
diegogarrido <chicodie04@gmail.com>
2016-08-14 13:34:53 -0300

    Tareas

--- tareas/tarea1508/programa02.java
+++ tareas/tarea1508/programa02.java
@@ -17,24 +17,25 @@ public class Tareametodos2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int m[][] = new int[3][3], confirm = 0;
+        int confirm = 0, M[][] = new int[3][3], n = 0;
+        System.out.println("Calculando cuadrado mágico...");
         while (confirm == 0) {
+            int m[][] = new int[3][3];
             m = rellenar(m);
             if (suma(m) == 8) {
                 confirm = 1;
+                M = m;
+                System.out.println("Intento N°: " + n);
             } else {
+                System.out.println("Intento N°: " + n + " fallido!");
                 confirm = 0;
             }
-            for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(m[i][j]);
-            }
-            System.out.println();
+            n++;
         }
-        }
+        System.out.println("Se consiguió un cuadrado mágico en el intento N° " + n);
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(m[i][j]);
+                System.out.print(M[i][j]);
             }
             System.out.println();
         }
@@ -45,8 +46,7 @@ public class Tareametodos2 {
             for (int j = 0; j < 3; j++) {
                 int confirm = 0;
                 while (confirm == 0) {
-                    Random r = new Random();
-                    m[i][j] = r.nextInt(9) + 1;
+                    m[i][j] = random();
                     if (comparar(m, m[i][j]) == 1) {
                         confirm = 1;
                     }
@@ -54,6 +54,13 @@ public class Tareametodos2 {
             }
         }
         return m;
+    }
+
+    public static int random() {
+        int n;
+        Random r = new Random();
+        n = r.nextInt(9) + 1;
+        return n;
     }
 
     public static int suma(int m[][]) {
